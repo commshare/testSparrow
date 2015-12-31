34bb8c3c3ea3ec3628e4a5d3eb81bd098e4ba1aa
commshare <commshare.zhang@gmail.com>
2015-12-31 11:30:55 +0800

    build jni ok with my Android.mk and Application.mk

--- ref/FFmpegMediaPlayer/gradle/fmp-library/library/src/main/jni/Application.mk
+++ ref/FFmpegMediaPlayer/gradle/fmp-library/library/src/main/jni/Application.mk
@@ -1,3 +1,12 @@
-# build with android-ndk-r9
+APP_ABI :=  armeabi-v7a
+#x86
+# arm64-v8a x86
+APP_PLATFORM := android-14
+APP_STL := gnustl_static
+#stlport_static
 
-APP_ABI := armeabi armeabi-v7a x86 mips
+ APP_CPPFLAGS := -std=c++11 -Wno-literal-suffix
+APP_CFLAGS := --std=c++11
+#APP_OPTIM  := debug
+#LOCAL_CPP_FEATURES += -Wliteral-suffix
+NDK_TOOLCHAIN_VERSION := 4.8
--- ref/FFmpegMediaPlayer/gradle/fmp-library/library/src/main/jni/player/wseemann_media_MediaPlayer.cpp
+++ ref/FFmpegMediaPlayer/gradle/fmp-library/library/src/main/jni/player/wseemann_media_MediaPlayer.cpp
@@ -29,6 +29,11 @@
 
 extern "C" {
     #include "ffmpeg_mediaplayer.h"
+
+    #ifndef INT64_C
+    #define INT64_C(c) (c ## LL)
+    #define UINT64_C(c) (c ## ULL)
+    #endif
 }
 
 // ----------------------------------------------------------------------------
